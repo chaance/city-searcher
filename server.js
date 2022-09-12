32df17fa638a591daf40dd8ed57a87475063b158
Chance Strickland <hi@chance.dev>
2022-09-12 14:28:01 -0400

    log address in server

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}`);
+  console.log(`http://localhost:${port}`);
 });
 
 function purgeRequireCache() {
